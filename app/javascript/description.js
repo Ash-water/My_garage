23890f78d49899a96720963121593a8014b98cba
Ash-water <mizuwater78@gmail.com>
2021-02-27 20:19:16 +0900

    artworks編集

--- app/javascript/description.js
+++ app/javascript/description.js
@@ -1,56 +1,64 @@
-const show_description = () => {
-  const artworks1 = document.getElementById('artworks1')
-  const artworks2 = document.getElementById('artworks2')
-  const artworks3 = document.getElementById('artworks3')
-  document.getElementById('bottles-desc').style.display = "none"
-  document.getElementById('pictweet-desc').style.display = "none"
-  document.getElementById('FURIMA-desc').style.display = "none"
-  document.getElementById('close-btn').style.display = "none"
-  const btn = document.getElementById('close-btn')
+const show_app = () => {
 
-  artworks1.addEventListener('click', function(){
-    document.getElementById('bottles-desc').style.display = "none"
-    document.getElementById('pictweet-desc').style.display = "none"
-    document.getElementById('FURIMA-desc').style.display = "none"
-    document.getElementById('close-btn').style.display = "none"
-    document.getElementById('bottles-desc').style.display = "block"
-    document.getElementById('close-btn').style.display = "block"
-  })
-  btn.addEventListener('click', function(){
-    document.getElementById('bottles-desc').style.display = "none"
-    document.getElementById('close-btn').style.display = "none"
-  })
-
-  artworks2.addEventListener('click', function(){
-    document.getElementById('bottles-desc').style.display = "none"
-    document.getElementById('pictweet-desc').style.display = "none"
-    document.getElementById('FURIMA-desc').style.display = "none"
-    document.getElementById('close-btn').style.display = "none"
-    document.getElementById('pictweet-desc').style.display = "block"
-    document.getElementById('close-btn').style.display = "block"
-  })
-  btn.addEventListener('click', function(){
-    document.getElementById('pictweet-desc').style.display = "none"
-    document.getElementById('close-btn').style.display = "none"
-  })
-
-  artworks3.addEventListener('click', function(){
-    document.getElementById('bottles-desc').style.display = "none"
-    document.getElementById('pictweet-desc').style.display = "none"
-    document.getElementById('FURIMA-desc').style.display = "none"
-    document.getElementById('close-btn').style.display = "none"
-    document.getElementById('FURIMA-desc').style.display = "block"
-    document.getElementById('close-btn').style.display = "block"
-  })
-  btn.addEventListener('click', function(){
-    document.getElementById('FURIMA-desc').style.display = "none"
-    document.getElementById('close-btn').style.display = "none"
-  })
+  const bottles = document.getElementById('bottles')
+  const pictweet = document.getElementById('pictweet')
+  const FURIMA = document.getElementById('FURIMA')
   
+  const show_b = document.getElementById('show-app1')
+  const show_p = document.getElementById('show-app2')
+  const show_f = document.getElementById('show-app3')
   
+  bottles.addEventListener('click', function(){
+    if  (show_b.getAttribute("style") == "display:block;") {
+      show_b.removeAttribute("style", "display:block;")
+    } else {
+      show_b.setAttribute("style", "display:block;")
+    }
+
+    if  (show_p.getAttribute("style") == "display:block;") {
+      show_p.removeAttribute("style", "display:block;")
+    }
+    
+    if  (show_f.getAttribute("style") == "display:block;") {
+      show_f.removeAttribute("style", "display:block;")
+    }
+  })
+
+  pictweet.addEventListener('click', function(){
+    if  (show_p.getAttribute("style") == "display:block;") {
+      show_p.removeAttribute("style", "display:block;")
+    } else {
+      show_p.setAttribute("style", "display:block;")
+    }
+
+    if  (show_b.getAttribute("style") == "display:block;") {
+      show_b.removeAttribute("style", "display:block;")
+    }
+
+    if  (show_f.getAttribute("style") == "display:block;") {
+      show_f.removeAttribute("style", "display:block;")
+    } 
+
+  })
+
+  FURIMA.addEventListener('click', function(){
+    if  (show_f.getAttribute("style") == "display:block;") {
+      show_f.removeAttribute("style", "display:block;")
+    } else {
+      show_f.setAttribute("style", "display:block;")
+    }
+
+    if  (show_p.getAttribute("style") == "display:block;") {
+      show_p.removeAttribute("style", "display:block;")
+    }
+    
+    if  (show_b.getAttribute("style") == "display:block;") {
+      show_b.removeAttribute("style", "display:block;")
+    }
+
+  })
 
 }
 
 
-
-window.addEventListener("load", show_description)
+window.addEventListener("load", show_app)
