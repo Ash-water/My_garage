0a29e61bdec0b218f531e1a127f7da74bb0f9834
Ash-water <mizuwater78@gmail.com>
2021-02-27 13:20:02 +0900

    調整

--- db/migrate/20210206123618_create_blogs.rb
+++ db/migrate/20210206123618_create_blogs.rb
@@ -1,8 +1,8 @@
 class CreateBlogs < ActiveRecord::Migration[6.0]
   def change
     create_table :blogs do |t|
-      t.text :title, null: false
-      t.string :content, null: false
+      t.string :title, null: false
+      t.text :content, null: false
       t.text :image
       t.timestamps
     end
